eb11c63ef8475df1678770731c7d86e9525d2724
Sean <conk3r_xd@hotmail.com>
2013-01-25 22:06:42 +0100

    sean animiatio

--- silence/silence/Animation.h
+++ silence/silence/Animation.h
@@ -1,21 +1,31 @@
 #ifndef INCLUDED_ANIMATION
 #define INCLUDED_ANIMATION
 
-#include <SFML\Graphics\Sprite.hpp>
-#include <SFML\System\Clock.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/System/Clock.hpp>
+#include "ImageManager.h"
 
-class Animation{
-private:
-	Animation(sf::Texture* texture, int frameTime, int frames);
+class ResourceManager
+
+class Animation
+{
+public:
+	Animation(const std::string& s, int frameTick, int nrFrames);
 	~Animation();
+
 	void update();
 	void setPosition(sf::Vector2f& pos);
-	sf::Sprite& getSprite();
-public:
-	sf::Sprite* mSprite;
-	sf::Clock mTimer;
-	int mFrameTime, mFrames, mCurrentFrame, mCurrentAnimation;
+	const sf::Sprite& getSprite() const;
 
+private:
+	sf::Clock d_frameTimer;		// Used for timing the frame switch
+	
+	int d_frameTick;
+	int d_nrFrames;
+	int d_currentFrame;
+
+	sf::Sprite d_sprite;
 };
 
 #endif
